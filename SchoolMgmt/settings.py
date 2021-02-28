e70d45c64acbfad092badf54a1ada6f8f52ed18e
bhuvi <bhuvionline@live.com>
2021-02-27 20:05:42 -0700

    Updated static settings

--- SchoolMgmt/settings.py
+++ SchoolMgmt/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFIlES = [os.path.join(BASE_DIR,   'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'Schools/static')
+STATICFIlES = [os.path.join(BASE_DIR,   'staticfiles')]
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Schools/static')]
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
